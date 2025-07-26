db873b54d2425a1cce84dd9e6d3c2efcda336cf2
Prajwal-palakshappa <prajwalpalakshappa2003@gmail.com>
2025-07-26 22:30:23 +0530

    Add files via upload

---
+++ conditionals.java
@@ -0,0 +1,24 @@
+public class conditionals { 
+    public static void main(String[] args) {
+        // int a=18;
+        // if(a>=18){
+        //     System.out.println("you can drive a car");
+        // }
+        // else{
+        //     System.out.println("no you not allowed to drive a car");
+        // }
+        int age=18;
+        boolean condi=(age==18);//this other method or else we can do the original method by using "==" equality check 
+        if(condi){
+            System.out.println("you are allowed to drive the car");
+
+        }
+        else{
+            System.out.println("you are not allowed to  drive the car");
+        }
+         
+
+    }
+    
+}
+// else is  not complusory only if run if fail the condition then it will not print anything
