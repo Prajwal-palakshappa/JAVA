0afe235183e943869a174d532f8b0599d69d842b
Prajwal-palakshappa <prajwalpalakshappa2003@gmail.com>
2025-07-26 22:30:56 +0530

    Add files via upload

---
+++ Index.java
@@ -0,0 +1,11 @@
+public class Index {
+    public static void main(String[] args) {
+        System.out.println("sum of this numbers");
+        int num1=10;
+        int num2=20;
+        int num3=30;
+        int sum=num1+num2+num3;
+        System.out.println(sum);
+    }
+    
+}
