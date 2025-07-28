feb5cc6dc13335323b2bba5eb3a25ea21df7541d
Prajwal-palakshappa <prajwalpalakshappa2003@gmail.com>
2025-07-28 23:36:24 +0530

    Add files via upload

---
+++ loops1.java
@@ -0,0 +1,14 @@
+public class loops1 {
+    public static void main(String[] args) {
+        int counter=0;
+        while(counter<100){
+            System.out.println("hello world");
+            counter++;
+           
+        }
+         System.out.println("printed hello world 100 times");
+    }
+    
+}
+// imppppp
+// keep in mind that while writing  the condition we should always not write the true condition 
